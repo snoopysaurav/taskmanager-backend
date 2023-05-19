206724a6c4d8d702c762a78810abc9831c1466b4
snoopysaurav <saurav2k60@gmail.com>
2023-05-19 18:37:17 +0545

    feat: signin, jwt

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -2,9 +2,15 @@ import { Request, Response } from "express";
 import { AppDatasource } from "../models/datasource";
 import { UserEntity } from "../models/user.entity";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+// Initialize Auth repository
 const AuthRepository = AppDatasource.getRepository(UserEntity);
 
+// Signup
 const signup = async (req: Request, res: Response) => {
   const user = new UserEntity();
 
@@ -36,8 +42,43 @@ const signup = async (req: Request, res: Response) => {
   }
 };
 
-const signin = async (req: Request, res: Response) => {};
+// Signin
+const signin = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user: any = await AuthRepository.findOneBy({
+      email: req.body.email,
+    });
 
+    if (!user) {
+      return res.status(500).json({ msg: `User doesnot exist..` });
+    }
+
+    if (email && password) {
+      const payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      const option = { expiresIn: "5d" };
+      const accessToken: any = jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        option
+      );
+      const compare = bcrypt.compareSync(password, user.password);
+      if (!compare) {
+        return res.status(404).json({ msg: `Incorrect Password` });
+      } else {
+        return res
+          .status(200)
+          .json({ msg: "Login successfull", accessToken, email: user.email });
+      }
+    }
+  } catch (error) {}
+};
+
+// Get All SignedUp Users
 const getUsers = async (req: Request, res: Response) => {
   try {
     const user = await AuthRepository.find();
