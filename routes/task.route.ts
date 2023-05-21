bfa2269f882ca57349f15bad2bde1fe463296562
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-05-21 09:09:53 +0000

    :sparkles: added middleware for auth

--- routes/task.route.ts
+++ routes/task.route.ts
@@ -6,10 +6,11 @@ import {
   postTask,
   updateTask,
 } from "../controllers/task.controller";
+import authMiddleware from "../middlewares/auth";
 
 const taskRouter = Router();
 
-taskRouter.route("/task").get(getAllTask).post(postTask);
+taskRouter.route("/task").get(getAllTask).post(authMiddleware,postTask);
 taskRouter.route("/task/:id").get(getTask).put(updateTask).delete(deleteTask);
 
 export default taskRouter;
